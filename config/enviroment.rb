9b7c24ab14d3e5985b8ecd3a8708cdf873ac0432
Thomas Pope <tpope0928@gmail.com>
2018-11-01 22:53:03 +0000

    enviroment setup

---
+++ config/enviroment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/Brewery_Search/Scraper'
+require_relative '../lib/Brewery_Search/Brewery'
+require_relative '../lib/Brewery_Search/CLI'
+require_relative '../lib/Brewery_Search
--- lib/Brewery_Search.rb
+++ lib/Brewery_Search.rb
@@ -3,3 +3,5 @@ require "Brewery_Search/version"
 module BrewerySearch
   # Your code goes here...
 end
+
+require_relative '../config/environment'
