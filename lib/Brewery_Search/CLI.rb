d10ae5a6dda988f6339e34fa19eb9683d75142ac
Thomas Pope <tpope0928@gmail.com>
2018-11-30 00:54:45 +0000

    CLI Work

--- lib/Brewery_Search/CLI.rb
+++ lib/Brewery_Search/CLI.rb
@@ -1,43 +1,21 @@
 class BrewerySearch::CLI
 
   def call
-    state_search
+    start
     list_breweries
-    info
-    goodbye
   end
 
-  def state_search
-    puts "Please enter the abbriviation of the state you would like to find breweries for:"
+  def start
+    puts "Welcome to the top 50 Breweries in the United States!"
+    puts "Here is a list of the top rated breweries according to UnTapped:"
   end
 
   def list_breweries
-    puts "Breweries!"
-
     @breweries = BrewerySearch::Brewery.all
-    @breweries.each.with_index(1) do |deal, i|
-      puts "#{i}. #{@breweries}"
+    @breweries.each.with_index(1) do |brewery, i|
+      puts "#{i}. #{deal.name} - #{deal.rating} - #{deal.num_beers}"
     end
   end
+    
 
-  def info
-    puts "Please enter the number for the brewery you would like more information about, list to go back to list of breweries, or type exit:"
-    input = nil
-    while input != "exit"
-      input = gets.strip.downcase
-
-      if input.to_i > 0
-        the_brewery = @breweries[input.to_i-1]
-        puts "#{the_brewery.name} - #{the_brewery.address} - #{the_brewery.phone} - #{the_brewery.url} - #{the_brewery.brewery_type}"
-      elsif input == "list"
-        list_breweries
-      else
-        puts "Not sure what you want, type list or exit."
-      end
-    end
-  end
-
-  def goodbye
-    puts "See you next time! Drink responsibly!"
-  end
 end
