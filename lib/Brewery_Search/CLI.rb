7b6810e3a570ae8c67a59caf05c719356e817c2e
Thomas Pope <tpope0928@gmail.com>
2018-11-30 01:19:15 +0000

    CLI Done

--- lib/Brewery_Search/CLI.rb
+++ lib/Brewery_Search/CLI.rb
@@ -3,6 +3,7 @@ class BrewerySearch::CLI
   def call
     start
     list_breweries
+    more_info
   end
 
   def start
@@ -13,9 +14,40 @@ class BrewerySearch::CLI
   def list_breweries
     @breweries = BrewerySearch::Brewery.all
     @breweries.each.with_index(1) do |brewery, i|
-      puts "#{i}. #{deal.name} - #{deal.rating} - #{deal.num_beers}"
+      puts "#{i}. #{brewery.name} - #{brewery.rating} - #{brewery.num_beers}"
     end
   end
-    
 
+  def more_info
+    input = nil
+    puts ""
+    puts "What restaurant would you like more information on?"
+    input = gets.strip.downcase
+
+    if input.to_i > 0
+        the_brewery = @breweries[input.to_i-1]
+        puts "#{brewery.name} - #{brewery.rating} - #{brewery.num_rating} - #{brewery.num_beers} - #{brewery.type}"
+        puts ""
+        puts "#{brewery.description}"
+        puts ""
+        puts "#{brewery.location} - #{brewery.phone} - #{brewery.website}
+      elsif input == "list"
+        list_breweries
+      elsif input == "exit"
+        quit
+      else
+        puts "Not sure what you want, type list or exit."
+      end
+    end
+  end
+
+  def quit
+    puts ""
+    puts " Have fun! Please drink responsibily!"
+  end
+
+
+
+
+    
 end
