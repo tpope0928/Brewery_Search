83422037da68ef2193139fd1271fe7c25c36b276
Thomas Pope <tpope0928@gmail.com>
2018-12-10 00:46:59 +0000

    new approach

--- lib/Brewery_Search/Brewery.rb
+++ lib/Brewery_Search/Brewery.rb
@@ -1,32 +1,23 @@
-class BrewerySearch::Brewery
+class Brewery
 
-  attr_accessor :name, :location, :type, :num_beers, :rating, :num_rating, :website, :description
+  attr_accessor :name, :num_beers, :rating, :num_rating
 
-  def self.all
-    self.scrape_breweries
-  end
+  @all = []
 
-  def self.scrape_breweries
-    breweries = []
-
-    breweries << self.scrape_untappd
-
-    breweries
-  end
-
-  def self.scrape_untappd
-    doc = Nokogiri::HTML(open("https://untappd.com/brewery/top_rated?country_id=86"))
-
-    brewery = self.new
-    brewery.name = doc.css("div.beer-item p.name").text.strip
-    #brewery.location = doc.css("div.brewery").text.strip
-    #brewery.type = doc.css("p.style")
-    brewery.num_beers = doc.css("div.beer-item p.abv").text.strip
-    brewery.rating = doc.css("div.beer-item p.rating span[num]").text.strip
-    brewery.num_rating = doc.css("div.beer-item p[ibu]").text.strip
-    #brewery.website = doc.search("a.url tip track-click")
-    #brewery.description = doc.css("div.beer-descrption-read-less")
-
-    brewery
+  def initialize
+    @all << self
   end
 end
+
+
+class Scraper
+
+  def self.scrape_data
+    doc =
+      Nokogiri::HTML(open("https://untappd.com/brewery/top_rated?country_id=86"))
+      doc.css(".beer-item").each do |brewery|
+        binding.pry
+      end
+
+    end
+  end
--- lib/Brewery_Search/CLI.rb
+++ lib/Brewery_Search/CLI.rb
@@ -1,47 +1,26 @@
-class BrewerySearch::CLI
+#class BrewerySearch::CLI
 
-  def call
-    start
-    list_breweries
-    more_info
-  end
+  #def call
+  #  start
+  #  list_breweries
+  #  more_info
+  #end
 
-  def start
-    puts "Welcome to the top 50 Breweries in the United States!"
-    puts "Here is a list of the top rated breweries according to UnTapped:"
-  end
+#  def start
+#    puts "Welcome to the top 50 Breweries in the United States!"
+#    puts "Here is a list of the top rated breweries according to UnTapped:"
+#  end
 
-  def list_breweries
-    @breweries = BrewerySearch::Brewery.all
-    @breweries.each.with_index(1) do |brewery, i|
-      puts "#{i}. #{brewery.name} "
-    end
-  end
+#  def list_breweries
+#    @breweries = BrewerySearch::Brewery.all
+#    @breweries.each.with_index(1) do |brewery, i|
+#      puts "#{i}. #{brewery.name} "
+#    end
+#  end
 
-  def more_info
-    input = nil
-    puts ""
-    puts "What restaurant would you like more information on?"
-    input = gets.strip
+#  def more_info
 
-    if input.to_i > 0
-        the_brewery = @breweries[input.to_i-1]
-        puts "#{the_brewery.name} - #{the_brewery.rating} - #{the_brewery.num_rating} - #{the_brewery.num_beers} - #{the_brewery.type}"
-        #puts ""
-        #puts "#{the_brewery.description}"
-        #puts ""
-        #puts "#{the_brewery.location} - #{the_brewery.phone} - #{the_brewery.website}"
-      elsif input == "list"
-        list_breweries
-      elsif input == "exit"
-        quit
-      else
-        puts "Not sure what you want, type list or exit."
-      end
-    end
-  end
-
-  def quit
-    puts ""
-    puts " Have fun! Please drink responsibily!"
-  end
+#  def quit
+#    puts ""
+#    puts " Have fun! Please drink responsibily!"
+#  end
