8477dde7f8cf0fe542b79e5016a52073877b429e
Thomas Pope <tpope0928@gmail.com>
2018-12-11 01:08:24 +0000

    CLI start 2:11:00

--- lib/Brewery_Search/CLI.rb
+++ lib/Brewery_Search/CLI.rb
@@ -3,9 +3,25 @@ class BrewerySearch::CLI
   def start
       puts "Welcome to the top 50 Breweries in the United States!"
       puts "Here is a list of the top rated breweries according to UnTapped:"
+      gets.strip
   end
 
+  #Brewery.all.each do |brewery|
+  #  brewery_details = [
+  #    " Name:",
+  #    " Rating:",
+  #    "",
+  #    "",
+  #  ]
+#
+#    puts "------------------------------------"
+#      brewery.instance_variables.each_with_index do |var, index|
+#        puts "#{brewery_details[index]} #{brewery.instance_variable_get(var)}"
+#      end
+#    puts "------------------------------------"
 
+
+#  end
 
 
 end
