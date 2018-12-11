9b654b95f2a21601679c781c6309f8b90cc21f2b
Thomas Pope <tpope0928@gmail.com>
2018-12-11 01:37:35 +0000

    List_breweries

--- lib/Brewery_Search/CLI.rb
+++ lib/Brewery_Search/CLI.rb
@@ -4,70 +4,22 @@ class BrewerySearch::CLI
       BrewerySearch::Scraper.scrape_data
       puts "Welcome to the top 50 Breweries in the United States!"
       puts "Here is a list of the top rated breweries according to UnTapped:"
-      BrewerySearch::Brewery.all.each do |brewery|
-        brewery_details = [
-          " Name:",
-          " Rating:",
-          "",
-          "",
-        ]
-      puts "------------------------------------"
-        brewery.instance_variables.each_with_index do |var, index|
-          puts "#{brewery_details[index]} #{brewery.instance_variable_get(var)}"
-        end
-      puts "------------------------------------"
+      list_breweries
     end
+  end
 
+  def list_breweries
+    BrewerySearch::Brewery.all.each do |brewery|
+      brewery_details = [
+        " Name:",
+        " Rating:",
+        "",
+        "",
+      ]
+    puts "------------------------------------"
+    brewery.instance_variables.each_with_index do |var, index|
+      puts "#{brewery_details[index]} #{brewery.instance_variable_get(var)}"
+    end
+    puts "------------------------------------"
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#class BrewerySearch::CLI
-
-  #def call
-  #  start
-  #  list_breweries
-  #  more_info
-  #end
-
-#  def start
-#    puts "Welcome to the top 50 Breweries in the United States!"
-#    puts "Here is a list of the top rated breweries according to UnTapped:"
-#  end
-
-#  def list_breweries
-#    @breweries = BrewerySearch::Brewery.all
-#    @breweries.each.with_index(1) do |brewery, i|
-#      puts "#{i}. #{brewery.name} "
-#    end
-#  end
-
-#  def more_info
-
-#  def quit
-#    puts ""
-#    puts " Have fun! Please drink responsibily!"
-#  end
