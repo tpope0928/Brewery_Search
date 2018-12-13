a45ac7cac77031ee989b15b01454024fa3b2916c
Thomas Pope <tpope0928@gmail.com>
2018-12-13 00:17:17 +0000

    back a bit 2:19:00

--- lib/Brewery_Search/CLI.rb
+++ lib/Brewery_Search/CLI.rb
@@ -19,35 +19,24 @@ class BrewerySearch::CLI
       puts "Welcome to the top 50 Breweries in the United States!"
       puts "Here is a list of the top rated breweries according to UnTapped:"
       list_breweries
-      menu
     end
   end
 
-  def list_breweries
-    @breweries.each do |brewery|
-      puts " "
-      puts "----------------------------------------"
-      print_details(brewery, 50, SIMPLE_DETAILS)
-      puts " "
-      puts "----------------------------------------"
-    end
-  end
+def list_breweries
+    BrewerySearch::Brewery.all.each do |brewery|
+      brewery_details = [
+    " Name:",
+    " Rating:",
+    "", #num_rating
+    "", #num_beers
+    ]
 
-  def print_details(brewery, number, details)
+    puts " "
+    puts "-----------------------------------------"
     brewery.instance_variables.each_with_index do |var, index|
-      if index < number
-        puts "#{details[index]} #{brewery.instance_variable_get(var)}"
+        puts "#{brewery_details[index]} #{brewery.instance_variable_get(var)}"
       end
     end
-  end
-
-  def menu
-    puts "Would you like more information?"
-    puts "Please put the number for the brewery you would like more information on:"
-    input = gets.strip_to_i
-    if input == 0 || input > 50 || input == nil
-      menu
-    end
-    print_details(@books[input], 50, MORE_DETAILS)
-    menu
+    puts " "
+    puts "-----------------------------------------"
   end
